35dc900f2b60c1005f8660bb48b8db0b9d77a786
shark1615 <rabbit212121@gmail.com>
2018-11-14 10:27:02 +0800

    Initial Commit

---
+++ lab09/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lab09
+//
+//  Created by Apple on 2018/11/14.
+//  Copyright © 2018年 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
