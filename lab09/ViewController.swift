43c421506aef6bdcc467a7af2b31a147bd1632e0
shark1615 <rabbit212121@gmail.com>
2018-11-21 06:08:25 +0800

    加分＿BMI

--- lab09/ViewController.swift
+++ lab09/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var weight: UITextField!
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func caculate(_ sender: Any) {
+        if let heightText = height.text, let weightText = weight.text,let height = Double(heightText),let weight = Double(weightText){
+            let heightmeter = height / 100
+            print(weight)
+            print(heightmeter)
+            let resultText =  weight / (heightmeter * heightmeter)
+            result.text = resultText.description
+        }
+        
+        
+        
+    }
 }
 
